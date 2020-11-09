ea043ba383ccd38a6ac7c001d6ba5ef2a66c456b
toab1111 <68186646+toab1111@users.noreply.github.com>
2020-11-09 11:43:00 +0700

    Add files via upload

--- hw_lisa_6030820921.m
+++ hw_lisa_6030820921.m
@@ -2,8 +2,6 @@
 clc
 clear
 format long g
-%% import shape file
-s = shaperead('elderly_bkk.shp');
 %% Contiguity Matrix
 %%ทุ่งพญาไท,ถนนพญาไท,ถนนเพชรบุรี,สี่แยกมหานาค,สวนจิตรลดา,ถนนนครไชยศรี,พญาไท,สามเสนใน
 w=[0 1 1 1 1 1 1 0  %%ทุ่งพญาไท
@@ -18,23 +16,10 @@ w=[0 1 1 1 1 1 1 0  %%ทุ่งพญาไท
 w_sum=sum(w,2);
 %% Row Standardized Spatial Weights Matrix
 wij=w./w_sum;
-%% เก็บแถวข้อมูลที่ต้องการ
-a=[s.rate];
-c=[s.OBJECTID];
-%% object id 
-object = [70 71 69 114 115 23 53 174];
-%% หา index ของ object id 
-index=[];
-for i=1:length(object)
-    index=[index,find(c==object(i))];
-end
-%% หา rate จาก index
-rate_c=[];
-for i=1:length(object)
-    rate_c=[rate_c,a(index(i))];
-end
+%% ค่า rate
+rate_c=[13.29,25.59,26.27,26.53,29.61,21.9,21.76,15.91];
 %% Z-Scores 
-Z0 = zscore(rate_c,0); % sample formula
+Z0 = zscore(rate_c,1); % population formula
 %% Anselin’s LISA (Local Moran’s I)
 I=Z0'.*wij*Z0';
 
